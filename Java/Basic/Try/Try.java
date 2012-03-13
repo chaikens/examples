138d77f506b352834f0183118045e76c3da16ace
Seth Chaiken <sdc@cs.albany.edu>
2012-03-12 21:14:24 -0400

    Started to add inputting.

--- Java/Basic/Try/Try.java
+++ Java/Basic/Try/Try.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 class Try
 {
   public static void main(String a[])
   {
     System.out.println("Hello world!");
+    Scanner sc = new Scanner(System.out);
+    int inInt;
   }
 }
